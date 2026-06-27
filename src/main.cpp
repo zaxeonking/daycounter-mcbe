77aa7dac8325d893db126fed0665cb4e67b5ce9a
zaxeonking <zaxeon5@gmail.com>
2026-06-28 02:27:07 +0700

    Implement eglSwapBuffers hook for logging
    Added eglSwapBuffers hook to log frame rendering with day count.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <jni.h>
 #include <android/log.h>
 #include <dlfcn.h>
+#include <EGL/egl.h>
 #include <string>
-#include <sstream>
 
 #define LOG_TAG "DayCounter"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -10,25 +10,22 @@
 typedef bool (*GlossInit_t)(bool);
 typedef void* (*pl_resolve_signature_t)(const char*, const char*);
 typedef int (*GlossHookFunction_t)(void*, void*, void**);
-
-// ImGui function types
-typedef void* (*ImGui_GetIO_t)();
-typedef void (*ImGui_Text_t)(const char*, ...);
-typedef bool (*ImGui_Begin_t)(const char*, bool*, int);
-typedef void (*ImGui_End_t)();
-typedef void (*ImGui_SetNextWindowPos_t)(float, float, int, float, float);
-typedef void (*ImGui_SetNextWindowBgAlpha_t)(float);
+typedef EGLBoolean (*eglSwapBuffers_t)(EGLDisplay, EGLSurface);
 
 GlossInit_t pGlossInit = nullptr;
 pl_resolve_signature_t pResolve = nullptr;
+GlossHookFunction_t pGlossHook = nullptr;
+eglSwapBuffers_t pOrigSwapBuffers = nullptr;
 
-// MCBE time signature dari ThirdPersonNametag
-const char* TIME_SIG = "? ? 40 F9 ? ? ? EB ? ? ? 54 ? ? 40 F9 ? 81 40 F9 E0 03 ? AA 00 01 3F D6 ? ? 00 37 ? ? 40 F9 ? ? ? A9 ? ? ? CB ? ? ? D3 ? ? 00 51 ? ? ? 8A";
+// Day counter variable
+static int dayCount = 0;
 
-void* pTimeFunc = nullptr;
-
-// Hook callback
-void* pOriginalTime = nullptr;
+// Our hooked eglSwapBuffers
+EGLBoolean mySwapBuffers(EGLDisplay display, EGLSurface surface) {
+    // Nanti kita tambah render ImGui di sini
+    LOGI("Frame rendered! Day: %d", dayCount++);
+    return pOrigSwapBuffers(display, surface);
+}
 
 __attribute__((constructor))
 void init() {
@@ -42,20 +39,31 @@ void init() {
 
     pGlossInit = (GlossInit_t)dlsym(preloader, "GlossInit");
     pResolve = (pl_resolve_signature_t)dlsym(preloader, "pl_resolve_signature");
+    pGlossHook = (GlossHookFunction_t)dlsym(preloader, "GlossHookFunction");
 
-    if (!pGlossInit || !pResolve) {
-        LOGI("Failed get functions!");
+    if (!pGlossInit) {
+        LOGI("Failed get GlossInit!");
         return;
     }
 
     pGlossInit(false);
 
-    // Resolve MCBE function
-    pTimeFunc = pResolve("libminecraftpe.so", TIME_SIG);
-    if (pTimeFunc) {
-        LOGI("Found MCBE time function at: %p", pTimeFunc);
-    } else {
-        LOGI("MCBE time function not found!");
+    // Hook eglSwapBuffers
+    void* egl = dlopen("libEGL.so", RTLD_NOW);
+    if (!egl) {
+        LOGI("libEGL FAILED: %s", dlerror());
+        return;
+    }
+
+    void* swapFunc = dlsym(egl, "eglSwapBuffers");
+    if (!swapFunc) {
+        LOGI("eglSwapBuffers not found!");
+        return;
+    }
+
+    if (pGlossHook) {
+        pGlossHook(swapFunc, (void*)mySwapBuffers, (void**)&pOrigSwapBuffers);
+        LOGI("eglSwapBuffers hooked!");
     }
 
     LOGI("=== DayCounter LOADED ===");
