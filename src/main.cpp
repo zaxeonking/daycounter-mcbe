2f141ad1934351c424786645a77ad198574de432
zaxeonking <zaxeon5@gmail.com>
2026-06-28 02:37:50 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,11 @@
 #include <android/log.h>
 #include <dlfcn.h>
 #include <EGL/egl.h>
+#include <GLES3/gl3.h>
 #include <string>
+#include <sstream>
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
 
 #define LOG_TAG "DayCounter"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -17,54 +21,46 @@ pl_resolve_signature_t pResolve = nullptr;
 GlossHookFunction_t pGlossHook = nullptr;
 eglSwapBuffers_t pOrigSwapBuffers = nullptr;
 
-// Day counter variable
-static int dayCount = 0;
+static bool imguiInitialized = false;
+static int screenW = 1080;
+static int screenH = 1920;
 
-// Our hooked eglSwapBuffers
+void initImGui() {
+    if (imguiInitialized) return;
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.DisplaySize = ImVec2(screenW, screenH);
+    ImGui::StyleColorsDark();
+    ImGui_ImplOpenGL3_Init("#version 100");
+    imguiInitialized = true;
+    LOGI("ImGui initialized %dx%d", screenW, screenH);
+}
+
 EGLBoolean mySwapBuffers(EGLDisplay display, EGLSurface surface) {
-    // Nanti kita tambah render ImGui di sini
-    LOGI("Frame rendered! Day: %d", dayCount++);
-    return pOrigSwapBuffers(display, surface);
-}
+    // Get screen size
+    EGLint w, h;
+    eglQuerySurface(display, surface, EGL_WIDTH, &w);
+    eglQuerySurface(display, surface, EGL_HEIGHT, &h);
+    screenW = w; screenH = h;
 
-__attribute__((constructor))
-void init() {
-    LOGI("=== DayCounter LOADING ===");
+    initImGui();
 
-    void* preloader = dlopen("libpreloader.so", RTLD_NOW);
-    if (!preloader) {
-        LOGI("preloader FAILED: %s", dlerror());
-        return;
-    }
+    // Render ImGui
+    ImGuiIO& io = ImGui::GetIO();
+    io.DisplaySize = ImVec2(w, h);
 
-    pGlossInit = (GlossInit_t)dlsym(preloader, "GlossInit");
-    pResolve = (pl_resolve_signature_t)dlsym(preloader, "pl_resolve_signature");
-    pGlossHook = (GlossHookFunction_t)dlsym(preloader, "GlossHookFunction");
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui::NewFrame();
 
-    if (!pGlossInit) {
-        LOGI("Failed get GlossInit!");
-        return;
-    }
+    // DayCounter window
+    ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_Always);
+    ImGui::SetNextWindowBgAlpha(0.5f);
+    ImGui::Begin("##dc", nullptr,
+        ImGuiWindowFlags_NoTitleBar |
+        ImGuiWindowFlags_NoResize |
+        ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_AlwaysAutoResize);
 
-    pGlossInit(false);
-
-    // Hook eglSwapBuffers
-    void* egl = dlopen("libEGL.so", RTLD_NOW);
-    if (!egl) {
-        LOGI("libEGL FAILED: %s", dlerror());
-        return;
-    }
-
-    void* swapFunc = dlsym(egl, "eglSwapBuffers");
-    if (!swapFunc) {
-        LOGI("eglSwapBuffers not found!");
-        return;
-    }
-
-    if (pGlossHook) {
-        pGlossHook(swapFunc, (void*)mySwapBuffers, (void**)&pOrigSwapBuffers);
-        LOGI("eglSwapBuffers hooked!");
-    }
-
-    LOGI("=== DayCounter LOADED ===");
-}
+    // Get day from MCBE (placeholder dulu)
+    ImGui::TextColored
